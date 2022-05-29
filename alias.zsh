04770ddc6ceb02397c7f066988990d622cb1b38a
Juo-Tung Chen <jjcc0704@gmail.com>
2022-05-29 19:04:59 +0800

    - remove zsh-completions from .zshrc - add alias.zsh to dotfiles - edit .zshrc to source alias.zsh in dotfiles

---
+++ alias.zsh
@@ -0,0 +1,15 @@
+#custom alias
+
+alias gohome="cd ~"
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="cd ~/.oh-my-zsh"
+alias aliasconfig="vim ~/Documents/dotfiles/alias.zsh"
+alias vimconfig="vim ~/.vimrc"
+alias tmuxconfig="vim ~/.tmux.conf"
+alias gs="git status"
+alias gc="git commit"
+alias v="vim"
+
+# Save you from mistyping
+alias sl="ls"
+alias dc="cd"
