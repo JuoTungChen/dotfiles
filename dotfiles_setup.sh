5415a13922255dd65eb1091a368381d55547cc8c
JuoTungChen <chenjuotung@gmail.com>
2023-01-23 14:19:11 -0500

    - add automatic install for git zsh vim and tmux

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# install git zsh vim
+sudo apt-get install git-all zsh vim tmux
+chsh -s $(which zsh)    # set zsh as default shell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"   # install oh-my-zsh
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k       # install powerlevel10k
+git clone https://github.com/sickill/vim-monokai.git ~/.vim/colors                            # install monokai colorscheme for vim
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
 Dotfiles=(".vimrc" ".zshrc" ".tmux.conf")
 
 dir="${HOME}/Documents/dotfiles"
