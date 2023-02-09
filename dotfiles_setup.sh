cd86261c18f98debd91fc35b9ef0aaa31185c936
JuoTungChen <chenjuotung@gmail.com>
2023-02-09 09:18:26 -0500

    fix bug and add vscode auto install

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # install git zsh vim
-sudo apt-get install git-all zsh vim-gtk3 tmux
+sudo apt install curl git-all zsh vim-gtk3 tmux 
 chsh -s $(which zsh)    # set zsh as default shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"   # install oh-my-zsh
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k       # install powerlevel10k
-git clone https://github.com/sickill/vim-monokai.git ~/.vim/colors                            # install monokai colorscheme for vim
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/sickill/vim-monokai.git ~/.vim/                                  # install monokai colorscheme for vim
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sudo snap install --classic code                                                              #install vscode
 
+# cp ~/Documents/dotfiles/settings.json ./  
 
 Dotfiles=(".vimrc" ".zshrc" ".tmux.conf")
 
@@ -18,3 +20,5 @@ for dotfile in "${Dotfiles[@]}"; do
 	echo "creating symlinks for $dotfile"
 	ln -sf $dir/$dotfile ${HOME}/${dotfile} 
 done
+
+ln -sf $dir/settings.json /home/justin/.config/Code/User/settings.json
