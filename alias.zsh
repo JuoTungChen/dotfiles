c9443cfa3212c5d068fb8d09412b90ea5bc57ef7
JuoTungChen <chenjuotung@gmail.com>
2023-02-21 13:58:26 -0500

    few adjustments

--- alias.zsh
+++ alias.zsh
@@ -11,7 +11,11 @@ alias gc="git commit"
 alias v="vim"
 alias sss="source install/setup.zsh"
 alias rr="ros2 run"
+alias ccc="colcon build"
+
+alias kb="bash ~/Documents/dotfiles/keyboard_switch.sh"
 
 # Save you from mistyping
 alias sl="ls"
 alias dc="cd"
+
